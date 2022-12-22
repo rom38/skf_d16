3af52f4a1151475bfbae66afb20f27c346b980d1
Rman vsc <rom38@users.noreply.github.com>
2022-12-22 08:03:26 +0000

    add app articles

---
+++ artboard/articles/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+
+from django.db import models
+
+# Create your models here.
+
+
+class Article(models.Model):
+    TYPE = (
+        ('tank', 'Танки'),
+        ('medic', 'Хилы'),
+        ('dd', 'ДД'),
+        ('privater', 'Торговцы'),
+        ('gildemaster', 'Гилдмастеры'),
+        ('quest', 'Квестгиверы'),
+        ('smith', 'Кузнецы'),
+        ('tanner', 'Кожевники'),
+        ('potion', 'Зельевары'),
+        ('spellmaster', 'Мастера заклинаний'),
+    )
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    category = models.CharField(max_length=8, choices=TYPE, default='tank')
+    upload = models.FileField(upload_to='uploads/')
+
+
+class UserResponse(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
