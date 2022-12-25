6e54bfe603ee74c2a9900600643ed3b431b7d8d7
Rman vsc <rom38@users.noreply.github.com>
2022-12-25 09:23:49 +0000

    register articles models in admin

--- artboard/articles/admin.py
+++ artboard/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
+
+from .models import Article
+from .models import UserResponse
 
 # Register your models here.
+
+admin.site.register(Article, MarkdownxModelAdmin)
+admin.site.register(UserResponse)
