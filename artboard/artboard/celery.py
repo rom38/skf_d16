7e63da3451e2f865bfe210a2bbf57fcc5f5f9e37
Rman vsc <rom38@users.noreply.github.com>
2023-01-11 14:58:22 +0000

    change periodic notify to 2 min for debug

--- artboard/artboard/celery.py
+++ artboard/artboard/celery.py
@@ -14,8 +14,8 @@ app.conf.beat_schedule = {
         'task': 'articles.tasks.news_notify_weekly_task',
         # еженедельная рассылка с последними новостями
         # (каждый понедельник в 8:00 утра).
-        'schedule': crontab(day_of_week="mon", hour="08", minute="00"),
+        # 'schedule': crontab(day_of_week="mon", hour="08", minute="00"),
         # рассылка каждые 2 минуты
-        # 'schedule': crontab(minute='*/2'),
+        'schedule': crontab(minute='*/2'),
     },
 }
